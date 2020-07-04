18547e7d4f54a6c46685c01c0ded8ff8c4da875a
piratach <piratach@gmail.com>
2020-07-04 17:30:50 +0700

    added main for interface and a new TODO list

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/****************************************************************************
+ * 0. Bug check (renaming + reverting...)
+ * 1. Build an interface using SFML
+ * 2. "Ignore" list
+ * 3. Keep track if a dir has already been created or not for efficiency
+ * 4. Wrap it up as an "app" available for download
+ ****************************************************************************/
+
+/****************************************************************************
+ * Interface (look at sfml-practice for reference)
+ * TODO: Use Moom's size as reference for how big the window should be
+ * TODO: Mac's default "X" button to close window but not the programme running
+ * TODO: Auto, manual/revert button
+ * TODO: Settings - with the ability to define groupings
+ *                  (e.g. audio - .mid, .mp3, .wav)
+ *                - add extensions
+ *                - what to do with "others" (ignore/add to "others' folder)
+ ****************************************************************************/
+
+
+#include "directory.hpp"
+
+int main() {
+  return 0;
+}

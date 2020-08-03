6b9d004e93a8bbefaffdfbbe1385e49359e2191e
piratach <piratach@gmail.com>
2020-08-04 00:26:38 +0700

    added xml functions for scene.cpp

--- src/xmlhelper.cpp
+++ src/xmlhelper.cpp
@@ -24,6 +24,7 @@ int getTextAttr(tinyxml2::XMLElement *pElement, int &size, int &x, int &y) {
   return 0;
 }
 
+/* Circular buttons */
 int getButtonAttr(tinyxml2::XMLElement *pElement, int &x, int &y, 
     int &r1, int &r2, bool &p) {
   pElement->QueryIntAttribute("x", &x);
@@ -34,11 +35,43 @@ int getButtonAttr(tinyxml2::XMLElement *pElement, int &x, int &y,
   return 0;
 }
 
+/* Rectangular buttons */
+int getButtonAttr(tinyxml2::XMLElement *pElement, int &x, int &y, int &w, 
+    int &h, std::string &s1, std::string &s2, int &fontSize) {
+  pElement->QueryIntAttribute("x", &x);
+  pElement->QueryIntAttribute("y", &y);
+  pElement->QueryIntAttribute("width", &w);
+  pElement->QueryIntAttribute("height", &h);
+  s1 = pElement->Attribute("idleText");
+  s2 = pElement->Attribute("activeText");
+  pElement->QueryIntAttribute("fontSize", &fontSize);
+  return 0;
+}
+
 int getTabLineAttr(tinyxml2::XMLElement *pElement, int &w, int &h, int &x,
     int &y) {
   pElement->QueryIntAttribute("width", &w);
   pElement->QueryIntAttribute("height", &h);
   pElement->QueryIntAttribute("x", &x);
   pElement->QueryIntAttribute("y", &y);
+  return 0;
+}
+
+int getColour(tinyxml2::XMLElement *pElement, int &r, int &g, int &b) {
+  pElement->QueryIntAttribute("r", &r);
+  pElement->QueryIntAttribute("g", &g);
+  pElement->QueryIntAttribute("b", &b);
+  return 0;
+}
+
+int getColour(tinyxml2::XMLElement *pElement, unsigned char &r, 
+    unsigned char &g, unsigned char &b) {
+  int r2, g2, b2;
+  pElement->QueryIntAttribute("r", &r2);
+  pElement->QueryIntAttribute("g", &g2);
+  pElement->QueryIntAttribute("b", &b2);
+  r = r2;
+  g = g2;
+  b = b2;
   return 0;
 }
--- src/xmlhelper.hpp
+++ src/xmlhelper.hpp
@@ -12,5 +12,13 @@ int getTextAttr(tinyxml2::XMLElement *pElement, int &size, int &x, int &y);
 int getButtonAttr(tinyxml2::XMLElement *pElement, int &inrad, int &outrad,
     int &x, int &y, bool &p);
 
+int getButtonAttr(tinyxml2::XMLElement *pElement, int &x, int &y, int &w, 
+    int &h, std::string &s1, std::string &s2, int &fontSize);
+
 int getTabLineAttr(tinyxml2::XMLElement *pElement, int &w, int &h, int &x,
     int &y);
+
+int getColour(tinyxml2::XMLElement *pElement, int &r, int &g, int &b);
+
+int getColour(tinyxml2::XMLElement *pElement, unsigned char &r, 
+    unsigned char &g, unsigned char &b);
